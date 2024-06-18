10e56f2960e5db75ec413df93204d88bc7b832f4
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-18 11:14:02 +0200

    Updated .gitignore and prisma schema, seeded to database

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -10,23 +10,23 @@ type Props = {
 
 const PostCard = ({ img, postTitle, description }: Props) => {
 	return (
-    <Link href={`/${postTitle}`}>
-		<section className='cursor-pointer'>
-			<div className='mt-12'>
-				<div>
-					<Image
-						src={img}
-						alt='Post Image'
-						width={300}
-						height={200}
-						className='rounded-[5px] max-w-1/2 md:max-w-[20vw]'
-					/>
+		<Link href={`/${postTitle}`}>
+			<section className='cursor-pointer'>
+				<div className='mt-12'>
+					<div>
+						<Image
+							src={img}
+							alt='Post Image'
+							width={300}
+							height={200}
+							className='rounded-[5px] max-w-1/2 md:max-w-[20vw]'
+						/>
+					</div>
+					<h2 className='text-xl font-semibold mt-6'>{postTitle}</h2>
+					<p className='text-gray-500'>{description}</p>
 				</div>
-				<h2 className='text-xl font-semibold mt-6'>{postTitle}</h2>
-				<p className='text-gray-500'>{description}</p>
-			</div>
-		</section>
-    </Link>
+			</section>
+		</Link>
 	)
 }
 
