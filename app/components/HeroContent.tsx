686cef5db1d3f4b3c89ab150060a68f5c8a9594b
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-21 15:14:30 +0200

    Updated fonts in hero section

--- app/components/HeroContent.tsx
+++ app/components/HeroContent.tsx
@@ -3,13 +3,13 @@ import Tag from './Tag'
 
 const HeroContent = () => {
 	return (
-		<div className='absolute bottom-[20%] w-1/2 d:w-1/3 px-[20px] md:px-[70px]'>
+		<div className='absolute bottom-[20%] w-1/2 md:w-1/3 px-[20px] md:px-[70px]'>
 			<Tag tag='travel' />
 			<h1 className='font-lora text-hero-content-color text-2xl md:text-4xl my-6 mb-2'>
 				Richird Norton photorealistic
 				<br /> rendering as real photos
 			</h1>
-			<p className='text-hero-content-color text-sm md:text-lg'>
+			<p className='font-lora text-hero-content-color text-sm md:text-md'>
 				Progressively incentivize cooperative systems through technically sound functionalities. The
 				credibly productivate seamless data.
 			</p>
