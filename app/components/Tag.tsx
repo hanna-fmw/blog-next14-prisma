e60964d0b238f8f01b588c020b0c029023a1021c
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-21 20:27:33 +0200

    Adjusted margins

--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Tag = ({ tag }: Props) => {
 	return (
 		<span
-			className='text-tag-color text-xs md:text-[10px] rounded-[8px] px-[10px] py-[5px] font-semibold uppercase tracking-[1px] my-3'
+			className='text-tag-color text-xs md:text-[10px] rounded-[8px] px-[10px] py-[5px] font-semibold uppercase tracking-[1px] mx-1 my-3'
 			style={{ backgroundColor: 'rgba(255, 255, 255, 0.15)' }}>
 			{tag}
 		</span>
