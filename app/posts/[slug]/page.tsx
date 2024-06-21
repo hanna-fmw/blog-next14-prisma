eaca5c6e8aa128179ba790f2038bce324f6c4b87
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-21 15:50:45 +0200

    Removed comments

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -10,8 +10,8 @@ type Props = {
 
 export default async function PostPage({ params: { slug } }: Props) {
 	const post = await prisma.post.findUnique({
-		where: { slug: slug }, //där slug (post.slug) är lika med den slug som skickas in (params.slug)
-		include: { author: true }, //include author to access User model
+		where: { slug: slug },
+		include: { author: true },
 	})
 
 	return (
@@ -32,13 +32,12 @@ export default async function PostPage({ params: { slug } }: Props) {
 						</div>
 						<div>{post?.subheading}</div>
 						<Image
-							src={post?.image ? post.image : 'no image'} //måste ha en fallback image/sträng av något slag, null funkar inte
+							src={post?.image ? post.image : 'no image'}
 							alt='Post Image'
 							width={300}
 							height={200}
 						/>
 
-						{/* <Image src={post?.image || "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt='Post Image' width={300} height={200} /> */}
 						<div>{post?.content}</div>
 						<div>Author: {post?.author.name}</div>
 					</Link>
