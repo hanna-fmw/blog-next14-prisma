65cc9adf29f42b63b62862ab07ebeb153846a88c
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-21 15:10:55 +0200

    Added fonts to layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,21 @@
 import type { Metadata } from 'next'
-import { Inter, Roboto } from 'next/font/google'
+import { Inter, Roboto, Lora, League_Spartan } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500'] })
+const roboto = Roboto({
+	subsets: ['latin'],
+	weight: ['400', '500'],
+	variable: '--font-roboto',
+})
+const lora = Lora({ subsets: ['latin'], weight: ['400', '500'], variable: '--font-lora' })
+
+const leage_spartan = League_Spartan({
+	subsets: ['latin'],
+	weight: ['400', '500'],
+	variable: '--font-spartan',
+})
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -18,7 +29,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={`${roboto.className} first-page`}>{children}</body>
+			<body className={`${roboto.variable} ${lora.variable} ${leage_spartan.variable}`}>
+				{children}
+			</body>
 		</html>
 	)
 }
