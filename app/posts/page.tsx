f080ecf636483f5af7016cf39b72662614728e42
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-22 20:54:10 +0200

    Removed redundant types

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,27 +6,6 @@ import Footer from '../components/Footer'
 import prisma from '@/lib/db'
 import Link from 'next/link'
 
-type Post = {
-	id: string
-	title: string
-	slug: string
-	subheading: string
-	content: string
-	tags: string[]
-	published: boolean
-	date: Date
-	author: User
-	authorId: string
-	image: string
-}
-
-type User = {
-	id: string
-	email: string
-	name: string
-	posts: Post[]
-}
-
 type Props = {
 	searchParams: { tagFilter: string }
 }
@@ -41,27 +20,30 @@ const PostsPage = async ({ searchParams: { tagFilter } }: Props) => {
 
 	const postCount = await prisma.post.count()
 
-	console.log('tagFilter:', tagFilter)
-	console.log(
-		'tags of all posts:',
-		posts.map((post) => post.tags)
-	)
-	console.log(
-		'type of tags:',
-		posts.map((post) => typeof post.tags)
-	)
+	// console.log('tagFilter:', tagFilter)
+	// console.log(
+	// 	'tags of all posts:',
+	// 	posts.map((post) => post.tags)
+	// )
+	// console.log(
+	// 	'type of tags:',
+	// 	posts.map((post) => typeof post.tags)
+	// )
 
 	const filteredPosts = tagFilter ? posts.filter((post) => post.tags.includes(tagFilter)) : posts
 
 	return (
 		<main>
-			<section className='w-screen min-w-full h-[80vh] bg-hero-bg bg-center object-cover relative'>
+			<section className='w-screen min-w-full h-[65vh] md:h-[70vh] bg-hero-bg bg-center object-cover relative'>
 				<Navbar />
-				<HeroContent />
+				<HeroContent
+					postTitle='Richird Norton photorealistic rendering as real photos.'
+					subheading='Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data.'
+				/>
 			</section>
 
 			<section className='w-screen px-[20px] md:px-[70px]'>
-				<h1 className='mt-12 md:mt-20 font-lora'>Posts ({postCount})</h1>
+				<h1 className='text-xl md:text-2xl mt-12 md:mt-18 font-lora'>Posts ({postCount})</h1>
 				<div>
 					<Link
 						href='/posts'
@@ -77,7 +59,7 @@ const PostsPage = async ({ searchParams: { tagFilter } }: Props) => {
 						</Link>
 					))}
 				</div>
-				<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6'>
+				<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-6'>
 					{filteredPosts.map((post) => (
 						<PostCard key={post.id} post={post} />
 					))}
