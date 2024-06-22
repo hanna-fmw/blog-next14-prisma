e60cad4b8793a6d9dbdd3086d1282daee9c3ddb5
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-22 20:42:33 +0200

    Added default value to tag prop and updated styels

--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
 type Props = {
-	tag: string
+	tag?: string
 }
 
-const Tag = ({ tag }: Props) => {
+const Tag = ({ tag = 'Explore the World' }: Props) => {
 	return (
-		<span
-			className='text-tag-color text-xs md:text-[10px] rounded-[8px] px-[10px] py-[5px] font-semibold uppercase tracking-[1px] mx-1 my-3'
-			style={{ backgroundColor: 'rgba(255, 255, 255, 0.15)' }}>
-			{tag}
-		</span>
+		<>
+			<span
+				className='text-tag-color text-xs md:text-sm rounded-[8px] px-[10px] py-[5px] font-semibold uppercase tracking-[1px] mr-1 my-3'
+				style={{ backgroundColor: 'rgba(255, 255, 255, 0.15)' }}>
+				{tag}
+			</span>
+		</>
 	)
 }
-
 export default Tag
