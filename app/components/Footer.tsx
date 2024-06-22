123e463b7ac1586f7523bc1806359a15d8615bdf
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-22 07:41:46 +0200

    Changed font size in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,21 +5,21 @@ const Footer = () => {
 		<footer className='w-screen py-10 mt-24 bg-[#212529]'>
 			<section className='w-full py-10 grid grid-cols-3 font-lora text-white justify-center'>
 				<div className='mx-auto'>
-					<h2 className='text-[14px] mb-2 md:text-[18px]'>Contact</h2>
+					<h2 className='text-[14px] mb-2 md:text-2xl'>Contact</h2>
 					<ul>
 						<li>info@blog.com</li>
 						<li>+944 450 904 505</li>
 					</ul>
 				</div>
 				<div className='mx-auto'>
-					<h2 className='text-[14px] mb-2 md:text-[18px]'>Legal</h2>
+					<h2 className='text-[14px] mb-2 md:text-2xl'>Legal</h2>
 					<ul>
 						<li>Terms</li>
 						<li>Privacy</li>
 					</ul>
 				</div>
 				<div className='mx-auto'>
-					<h2 className='text-[14px] mb-2 md:text-[18px]'>Connect</h2>
+					<h2 className='text-[14px] mb-2 md:text-2xl'>Connect</h2>
 					<ul>
 						<li>Facebook</li>
 						<li>Twitter</li>
