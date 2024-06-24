646a0ec0f61e90a4b9e88928635dca45085b72bd
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-24 11:31:08 +0200

    Replaced Navbar icons and updated Navbar styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
-import Image from 'next/image'
+import { SlSocialTwitter } from 'react-icons/sl'
+import { SlSocialFacebook } from 'react-icons/sl'
+import { SlSocialYoutube } from 'react-icons/sl'
+import { IoMdGlobe } from 'react-icons/io'
 
 const Navbar = () => {
 	return (
 		<div
 			className='w-screen flex justify-between items-center px-[20px] md:px-[70px] h-20'
 			style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-			<h2 className='font-spartan text-sm md:text-[20px] font-semibold text-navbar-link-color hover:text-navbar-link-hover-color'>
-				BLOG
-			</h2>
+			<Link
+				href='/'
+				className='cursor-pointer font-semibold text-navbar-link-color hover:text-navbar-link-hover-color'>
+				<IoMdGlobe size={25} />
+			</Link>
 
 			<nav className='flex gap-6 items-center'>
 				<Link
@@ -30,17 +35,17 @@ const Navbar = () => {
 				<Link
 					href='/'
 					className='w-[10px] h-[10px] sm:w-[14px] sm:h-[14px] md:w-[16px] md:h-[16px] text-navbar-link-color hover:text-navbar-link-hover-color'>
-					<Image src='/facebook.png' width={20} height={20} alt='Facebook logo' />
+					<SlSocialFacebook size={15} />
 				</Link>
 				<Link
 					href='/'
 					className='w-[10px] h-[10px] sm:w-[14px] sm:h-[14px] md:w-[16px] md:h-[16px] text-navbar-link-color hover:text-navbar-link-hover-color'>
-					<Image src='/twitter.png' width={20} height={20} alt='Twitter logo' />
+					<SlSocialTwitter size={15} />
 				</Link>
 				<Link
 					href='/'
 					className='w-[10px] h-[10px] sm:w-[14px] sm:h-[14px] md:w-[16px] md:h-[16px] text-navbar-link-color hover:text-navbar-link-hover-color'>
-					<Image src='/youtube.png' width={20} height={20} alt='YouTube logo' />
+					<SlSocialYoutube size={15} />
 				</Link>
 			</nav>
 		</div>
