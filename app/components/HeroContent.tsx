380cda2fa4c1ee56875aaab9f332fae5d986f6fb
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-24 11:35:22 +0200

    Added several props to HeroContent for reusability in both Hero sections and Related Posts sections

--- app/components/HeroContent.tsx
+++ app/components/HeroContent.tsx
@@ -6,19 +6,32 @@ type Props = {
 	subheading?: string
 	tag?: string
 	author?: string
+	contentClassName?: string
+	titleClassName?: string
+	textClassName?: string
+	subheadingClassName?: string
+	showTag?: boolean
 }
-
-const HeroContent = ({ postTitle, subheading, tag, author }: Props) => {
+// <div className='absolute bottom-[10%] w-1/2 md:w-1/2 lg:w-1/3 px-[20px] md:px-[70px]'>
+const HeroContent = ({
+	postTitle,
+	subheading,
+	tag,
+	author,
+	contentClassName,
+	titleClassName,
+	textClassName,
+	subheadingClassName,
+	showTag = true,
+}: Props) => {
 	return (
-		<div className='absolute bottom-[10%] w-1/2 md:w-1/2 lg:w-1/3 px-[20px] md:px-[70px]'>
-			<Tag tag={tag} />
-			<h1 className='font-lora text-hero-content-color text-2xl md:text-4xl my-6 mb-2'>
-				{postTitle}
-			</h1>
-			<p className='font-lora mt-3 text-hero-content-color text-sm md:text-base'>{subheading}</p>
-			<p className='font-lora mt-3 text-hero-content-color font-bold text-sm md:text-base'>
-				By {author}
+		<div className={`absolute ${contentClassName}`}>
+			{showTag && <Tag tag={tag} />}
+			<h1 className={`font-lora text-hero-content-color ${titleClassName}`}>{postTitle}</h1>
+			<p className={`font-lora mt-3 text-hero-content-color ${subheadingClassName}`}>
+				{subheading}
 			</p>
+			<p className={`font-lora mt-3 text-hero-content-color ${textClassName}`}>{author}</p>
 		</div>
 	)
 }
