e89785ec67532cdce1462d45d41f2c4652053ba0
Hanna Hosk <hanna.hosk@gmail.com>
2024-07-16 20:10:15 +0200

    Setup for Supabase auth

---
+++ utils/supabase/client.ts
@@ -0,0 +1,9 @@
+//For authentication (from: https://supabase.com/docs/guides/auth/server-side/nextjs)
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+	return createBrowserClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	)
+}
